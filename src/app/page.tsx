c305992e3cee9408fae16f5e76f61e131640a999
Miguel <lmmiguelgo@gmail.com>
2025-12-30 18:03:20 -0800

    Started Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,62 @@
+'use client';
+
+import { Pic } from "@/components/ui/pic/pic";
+import { useEffect } from "react";
 import Image from "next/image";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 
 export default function Home() {
+
+  // GSAP Animation
+
+  useEffect(() => {
+        //Registring ScrollTrigger Plugin
+        gsap.registerPlugin(ScrollTrigger);
+
+            const tl = gsap.timeline({
+            scrollTrigger: {
+                scrub: 1,
+                start: "top top",
+                end: "+=2000",
+                trigger: "body",
+            }
+        });
+
+        
+
+
+        tl.to('#main-background', {
+            scale: 1,
+            opacity: 0,
+            ease: "circ",
+
+        }, "<");
+
+         
+
+        
+
+
+
+    }, [])
+
+
+
+
+
   return (
-    <></>
+    <>
+
+
+    <div  className="fixed w-full h-screen overflow-hidden ">
+      <Image id="main-background" className="w-full h-full scale-125 opacity-88 pointer-events-none  " src={Pic.Background} alt="Background" />
+
+      
+    </div>
+
+
+    </>
   );
 }
--- src/components/ui/pic/pic.tsx
+++ src/components/ui/pic/pic.tsx
@@ -1,7 +1,9 @@
 import Logowide from '@/assets/logo-wide.png'
+import background from '@/assets/background.jpg'
 
 
 export const Pic = {
     LogoWide: Logowide,
+    Background: background, 
 }
 
